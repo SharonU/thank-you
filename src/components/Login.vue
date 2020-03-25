046b4b3057666830839be19f5d451fc115946cbd
Michael Akrawi <akrawimichael@gmail.com>
2020-03-25 17:02:36 +0200

    create app routes

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-wrapper">
+    <div class="img-wrapper"></div>
+    <div class="user-input-wrapper">
+      <h2>Thank You</h2>
+      <div>
+        <div
+          class="fb-login-button"
+          data-width
+          data-size="large"
+          data-button-type="continue_with"
+          data-layout="default"
+          data-auto-logout-link="false"
+          data-use-continue-as="false"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-wrapper {
+  display: flex;
+  flex-direction: row;
+}
+.user-input-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
